971c3b1edca79c3ceb4b3245c0db46a8a1c6c5e0
StillAyden <aydenshaw14@gmail.com>
2023-04-07 21:48:20 +0200

    TerrainMovement.cs Fix
    Spawning of terrain pieces now work.
    Will optimize methods next.
    
    Bugs:
    - When increasing speed, terrain pieces form gap between.

--- Assets/Scripts/TerrainControl.cs
+++ Assets/Scripts/TerrainControl.cs
@@ -6,37 +6,99 @@ public class TerrainControl : MonoBehaviour
 {
     [SerializeField] GameObject[] groundPrefabCollection;
     GameObject[] loadedGroundedPrefabs;
+    float[] tileSpawnPoints;
 
+    [Header("Spawning/Despawning Object")]
     [SerializeField] float groundPrefabSize = 10f;
+    [SerializeField] int prefabsLoaded = 5;
+    [SerializeField] float despawnPoint = -10;
+    
+    [Header("Movement")]
     [SerializeField] float moveSpeed = 1f;
+    private void Awake()
+    {
+        loadedGroundedPrefabs = new GameObject[prefabsLoaded];
+        tileSpawnPoints = new float[prefabsLoaded];
 
-
-    /// <summary>
-    GameObject thing1;
-    GameObject thing2;
-    /// </summary>
+        //Populate spawn points
+        for (int i = 0; i < prefabsLoaded; i++)
+        {
+            tileSpawnPoints[i] = i * groundPrefabSize;
+        }
+    }
     private void Start()
     {
-        thing1 = Instantiate(groundPrefabCollection[1]);
-        thing2 = Instantiate(groundPrefabCollection[1]);
-        Vector3 thing1Size = new Vector3(0, 0, groundPrefabSize);
-        thing2.transform.position = thing1.transform.position + thing1Size;
+        InitialTileSpawn();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        thing1.transform.position = new Vector3(thing1.transform.position.x, thing1.transform.position.y, thing1.transform.position.z + moveSpeed * -1 * Time.deltaTime);
+        TileSpawn();
+        MoveTiles();
+        DespawnTiles();
     }
 
     GameObject RandomPrefab()
     {
+        //Choose random prefab from List
         int random = Random.Range(0, groundPrefabCollection.Length);
-
         return groundPrefabCollection[random];
     }
 
-    void SpawnTiles()
+    void InitialTileSpawn()
     {
+        //Choosing Prefab and populating array
+        for (int k = 0; k < loadedGroundedPrefabs.Length; k++)
+        {
+            if (loadedGroundedPrefabs[k] == null)
+            {
+                //loadedGroundedPrefabs[k] = RandomPrefab();
+                loadedGroundedPrefabs[k] = Instantiate(RandomPrefab());
+                loadedGroundedPrefabs[k].transform.position = new Vector3( 0, 0, tileSpawnPoints[k]); 
+            }
+        }
+    }
 
+    void TileSpawn()
+    {
+        //Check if one of the loaded prefabs has been despawned, then add one
+        for (int k = 0; k < loadedGroundedPrefabs.Length; k++)
+        {
+            if (loadedGroundedPrefabs[k] == null)
+            {
+                loadedGroundedPrefabs[k] = Instantiate(RandomPrefab());
+                loadedGroundedPrefabs[k].transform.position = new Vector3(0, 0, groundPrefabSize * (prefabsLoaded - 1));
+            }
+        }
+    }
+
+    void DespawnTiles()
+    {
+        //Check if every prefab is infront of "despawnPoint", else destroy it
+        for(int k = 0; k < loadedGroundedPrefabs.Length; k++)
+        {
+            if (loadedGroundedPrefabs[k] != null)
+            {
+                if (loadedGroundedPrefabs[k].transform.position.z < despawnPoint)
+                {
+                    Destroy(loadedGroundedPrefabs[k]);
+                    loadedGroundedPrefabs[k] = null;
+                }
+            }
+        }
+    }
+
+    void MoveTiles()
+    {
+        //Go through all Loaded prefabs and move them
+        for (int k = 0; k < loadedGroundedPrefabs.Length; k++)
+        {
+            if (loadedGroundedPrefabs[k] != null)
+            {
+                loadedGroundedPrefabs[k].transform.position = new Vector3(loadedGroundedPrefabs[k].transform.position.x, 
+                                                                            loadedGroundedPrefabs[k].transform.position.y, 
+                                                                                loadedGroundedPrefabs[k].transform.position.z + moveSpeed * -1 * Time.deltaTime);
+            }
+        }
     }
 }
