f9c50b5496eac899290c7db405f7b1bb5065ee1d
StillAyden <aydenshaw14@gmail.com>
2023-04-11 10:58:22 +0200

    Minor Changes

--- Assets/Scripts/QuitGame.cs
+++ Assets/Scripts/QuitGame.cs
@@ -4,11 +4,8 @@ using UnityEngine;
 
 public class QuitGame : MonoBehaviour
 {
-        void Update()
+        public void Quit()
         {
-            if (Input.GetKeyDown(KeyCode.Escape))
-            {
                 Application.Quit();
-            }
         }
     }
